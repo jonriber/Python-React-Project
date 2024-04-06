169537e02c2aae4d72dfa8146c0072bf86c9dd0c
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-04-06 08:36:15 +0100

    authenticate api route

--- myproject/myapi/views.py
+++ myproject/myapi/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth import authenticate, login
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 # Create your views here.
 
 @api_view(['GET'])
@@ -10,3 +13,16 @@ def hello_world(request):
 @api_view(['GET'])
 def samuca(request):
     return Response({'message': 'Samuca!'})
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def authenticate(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return Response({'message': 'Authentication successful'})
+    else:
+        return Response({'message': 'Authentication failed'}, status=401)
